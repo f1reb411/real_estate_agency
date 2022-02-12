ce81fef5ea206aa48d6f5e704534416e909ffef0
Aleksandr Davydochkin <f1reb411111@gmail.com>
2022-02-12 21:35:12 +0300

    Add owners in flat page

--- property/admin.py
+++ property/admin.py
@@ -3,6 +3,12 @@ from django.contrib import admin
 from .models import Flat, Complaint, Owner
 
 
+class OwnerInline(admin.TabularInline):
+    model = Owner.flats.through
+    raw_id_fields = ['owner']
+    extra = 1
+
+
 @admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ['town', 'address', 'owner']
@@ -19,6 +25,7 @@ class FlatAdmin(admin.ModelAdmin):
     list_editable = ['new_building']
     list_filter = ['new_building', 'rooms_number', 'has_balcony']
     raw_id_fields = ['liked_by']
+    inlines = (OwnerInline,)
 
 
 @admin.register(Complaint)
