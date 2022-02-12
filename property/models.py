190d9e64eafda039581c49da6a562f29ad042950
Aleksandr Davydochkin <f1reb411111@gmail.com>
2022-02-12 17:27:37 +0300

    Add db model for likes

--- property/models.py
+++ property/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Flat(models.Model):
@@ -50,3 +51,19 @@ class Flat(models.Model):
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
+
+
+class Complaint(models.Model):
+    user = models.ForeignKey(
+        User,
+        null=True,
+        verbose_name='кто жаловался',
+        on_delete=models.SET_NULL
+    )
+    flat = models.ForeignKey(
+        Flat,
+        verbose_name='квартира, на которую пожаловались',
+        null=True,
+        on_delete=models.SET_NULL
+    )
+    text = models.TextField('текст', blank=True)
